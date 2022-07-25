f094bb138a4ce612654c02f2b9c31f58fbecb667
Ahmed Al-Jawahiry <ahmedaljawahiry@gmail.com>
2022-07-25 15:05:20 +0100

    frontend: vite, react, tailwind

---
+++ frontend/src/components/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+
+  return (
+    <div className="bg-black h-screen w-full flex justify-center items-center">
+      <h1 className="text-3xl text-red-700">WIP</h1>
+    </div>
+  )
+}
